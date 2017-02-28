0c2dfecc0e1279e0f144e1f0aa4887ffa1ed9b18
Sammy Steiner <sammysteiner@gmail.com>
2017-02-28 23:17:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,7 @@ def oxford_comma(array)
   if array.size <= 2
     array.join(" and ")
   else
-    last = array.pop
-    and_last = "and #{last}"
-    array.push(and_last)
+    array[-1].insert(0, "and ")
     array.join(", ")
   end
 end
