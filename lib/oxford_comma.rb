6bf394bb8839562fbd8786afa51521ea0a07ac20
Sammy Steiner <sammysteiner@gmail.com>
2017-02-28 23:13:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size <= 2
+    array.join(" and ")
+  else
+    last = array.pop
+    and_last = "and #{last}"
+    array.push(and_last)
+    array.join(", ")
+  end
 end
