b994419dbb186cf407736cfddf234d701038abf1
Zach Dubno <zachedubno@gmail.com>
2017-03-06 15:44:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    foo = ""
+    a = "and #{array.pop}"
+    array.each do |item|
+      foo << "#{item}, "
+    end
+    foo << a
+  end
 end
