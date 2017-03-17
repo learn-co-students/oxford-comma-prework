9ccf8ba3c9351a781d3e191fee1af1f776fd07e0
Kyle Stewart <kyle.w.stewart@mac.com>
2017-03-17 17:21:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    string = array[0]
+  when 2
+    string = array.join(" and ")
+  else
+    last = "and #{array.pop}"
+    array.push(last)
+    string = array.join(", ")
+  end
+  string
 end
+#
+# fruits = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+# # fruits =  ["kiwi", "durian"]
+# # fruits =  ["kiwi"]
+# #
+# puts oxford_comma(fruits)
