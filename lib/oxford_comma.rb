76719d26f5774e88a0b3120b8885d02c639f7694
Alla Gorik <allagorik2901@gmail.com>
2017-03-18 23:35:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size < 2
+    return array[0]
+  elsif array.size == 2
+    return array.join(' and ')
+  else
+    array << "and #{array.pop}"
+    return array.join(', ')
+end
 end
