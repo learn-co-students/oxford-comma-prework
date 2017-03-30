f0762ad5750075e992d06c876ea11cf9828457a7
brian-hopman <hopman_2@hotmail.com>
2017-03-29 23:32:59 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,41 @@
+#def oxford_comma(array)
+#  count = 0
+#  product = []
+#  endproduct = []
+#  ary = array.join(" ").split(" ")
+
+#  while count <= ary.length
+#      product << "#{ary[count]},"
+#      count += 1
+#     if count == (ary.length - 1)
+#       product << "and #{ary[count]}"
+#    end
+#  end
+
+#  return product.join(" ")
+#end
+
 def oxford_comma(array)
+
+  if array.length == 1
+    array.join
+
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+
+  elsif array.length >= 3
+    commas = []
+
+    array.each.with_index(1) { |e, i|
+      if i == array.length
+        commas << "and " + e
+      else commas << e + ", "
+
+      end
+
+    }
+    return commas.join
+  end
+
 
 end
