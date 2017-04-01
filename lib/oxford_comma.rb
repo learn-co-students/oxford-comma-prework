b6b29ebee7da17a8d207b514449822421f87b72f
Alexandra Williams <alexandrawilliams.aiw@gmail.com>
2017-04-01 17:55:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+if array.size == 1
+  return array.join
+elsif array.size == 2
+  return array.join(" and ")
+elsif array.size >= 3
+  longray = []
+  longray.push(array.first(array.size-1))
+  longray.push("and #{array.last}")
+  return longray.join(", ")
+else
+end
 
 end
