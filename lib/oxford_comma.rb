570b9d42084f8210a6d0fb390d0d6cff9bc8ebf7
Mike Luo <zluo@alumni.berklee.edu>
2017-04-03 00:25:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  oxford = array.pop
+  commas = array
+  if array.length == 0
+    return "#{oxford}"
+  elsif array.length == 1
+    return "#{commas.join} and #{oxford}"
+  else
+    return "#{commas.join(", ")}, and #{oxford}"
+  end
 end
