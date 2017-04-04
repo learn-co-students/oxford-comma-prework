058ad43ced6dbf037a397cd3d2dd64322292acfe
nkernis <nkernis@gmail.com>
2017-04-04 16:01:14 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+	if array.length == 1
+		array.join
+
+	elsif array.length == 2
+		array.insert(1, " and ")
+		array.join
+
+	else
+		last_item = "and "
+		last_item << array.pop
+		array.push(last_item)
+		array.join(", ")
+
+	end
+		
 end
