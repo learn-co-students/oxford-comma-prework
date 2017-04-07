6a72ebca19471387532da057bc7b19f25c79ccf3
Pradeep Dayaram <vickdayaram@gmail.com>
2017-04-07 16:38:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
 
+
+
+def oxford_comma(array)
+  if array.length == 1
+    return array.join()
+  end
+  if array.length == 2
+    array3 = []
+    array.insert(array.length - 1, "and")
+    array3 = array.pop(1)
+    return array.join(" ") + " " + array3.join(" ")
+  end
+  if array.length > 2
+    array2 = []
+    array.insert(array.length - 1, "and")
+    array2 = array.pop(2)
+    return array.join(", ") + ", " + array2.join(" ")
+  end
 end
