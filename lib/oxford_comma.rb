75eec1fe3572e9bd7582adf6e257215d7f26b188
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-04-09 15:39:56 -0500

    Olly olly oxen free

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length <= 2
+    array.join(" and ")
+  else
+    last_element = "and " + array.last
+    array[array.length - 1] = last_element
+    array.join(", ")
+  end
 end
