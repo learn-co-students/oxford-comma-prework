4a9b8c535e0231b414727c43d5cb3c60afd1aca9
Vivian Lin <lin.vvian@gmail.com>
2017-04-15 00:35:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    one = array.join
+  elsif array.size == 2
+    two = array.join(" and ")
+  else
+    list = array[0,array.length-1].join(", ")
+    list << ", and #{array.last}"
+  end
 end
