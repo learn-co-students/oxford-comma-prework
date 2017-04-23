0acbc275d4c9d10b9732dc25dd3ce386b729f71f
Catherine Lavin <cmlavin7@gmail.com>
2017-04-23 02:07:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+num = array.length
+  if num == 1
+    array.join
+  elsif num == 2
+    array.join(" and ")
+  else
+    array[0...-1].join(", ") + ", and #{array[-1]}"
+  end
 end
