130988c936bcc52ace390cf18c20d11ac1885494
Charles Wooley <cwooley89@gmail.com>
2017-04-28 16:41:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  outString = ""
+  if arr.size > 2   
+    arr.each_with_index do |word, index|
+      if arr[index + 1]
+        outString << "#{word}, "
+      else
+        outString << "and #{word}"
+      end
+    end
+  elsif arr.size == 2
+    outString = "#{arr[0]} and #{arr[1]}"
+  else
+    outString = arr.pop
+  end
+  outString
 end
