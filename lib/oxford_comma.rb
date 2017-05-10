b2bee6e717ca0cb3a857043d6f2d2d0e510c52a0
eli goldstein <elisings356@gmail.com>
2017-05-10 18:50:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  to_print = []
+
+  if array.length == 1
+      return array.join
+    elsif array.length == 2
+      return array.join(" and ")
+    elsif array.length == 3
+    array.insert(-2, "and")
+    foo = array.join(", ")
+    foo["and,"] = "and"
+    return foo
+      else
+        array.insert(-2, "and")
+        foo = array.join(", ")
+        foo["and,"] = "and"
+        return foo      
+end
 end
