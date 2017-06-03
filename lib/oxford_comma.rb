97226e36a28ce47105afaf205d35cc1cecc975af
Sho Miyata <sho.miyata.1@gmail.com>
2017-06-03 01:32:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    part1 = array.slice(0, array.length-1).join(", ")
+    part2 = array.slice(-1)
+    "#{part1}, and #{part2}"
+  end
 end
