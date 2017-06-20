d5e04fa8feab95021e98eddcbdc6a31a329f3e32
Steven Balasta <sbal13@gmail.com>
2017-06-20 02:48:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  string = ""
+  case array.length
+  when 1
+    string << array[0]
+  when 2
+    string << array[0] + " and " + array[1]
+  else
+    array.each{|x| x == array.last ? string << "and #{x}" : string << "#{x}, "}
+  end
+  string
 end
