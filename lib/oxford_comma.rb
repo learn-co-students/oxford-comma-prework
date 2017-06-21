6620d62bca3be4eec7f3e875235aa68c2e9bcfaf
Daniel Shiling <dshiling21@gmail.com>
2017-06-21 22:43:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
 
+  n = array.length
+
+  if n > 2
+    array.map do |el|
+      if el == array.last
+        'and ' + el
+      else
+        el + ','
+      end
+    end.join(" ")
+  elsif n == 2
+    array[0] + " and " + array[1]
+  else
+    array.first
+  end
+
+
 end
