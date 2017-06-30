2e7814ddfd6df012bc8875fec823c46aac049962
Justin Manalad <jstn.mnld@gmail.com>
2017-06-30 20:32:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
 
+  if array.length == 1
+    message = array[0]
+  elsif array.length == 2
+    message = array.join(" and ")    
+  elsif array.length >2
+    last = array.pop
+    message = array.join(", ")
+    message += ", and #{last}"
+  end
+
+  message
+  
 end
