509ef79453a4231ed28739694232ea4fd7b080ff
Logan Emerson <logemerson@gmail.com>
2017-07-01 18:12:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  ox_array = array
+  ox_string = ""
+  #ox_array.insert(-2, ", and")
+  #ox_string = ox_array.join(", ")
+  if ox_array.length > 2
+    ox_array[-1] = "and #{ox_array[-1]}"
+    ox_string = ox_array.join(", ")
+  elsif ox_array.length == 2
+    ox_array[-1] = " and #{ox_array[-1]}"
+    ox_array.join
+  else
+    ox_array.join
+  end
 end
