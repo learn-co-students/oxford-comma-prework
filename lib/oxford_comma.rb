50f30e84f5bb51b82d26369ea6a4a871ccbe029e
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-07-02 13:06:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
 
+  commas = ""
+
+  if array.length == 1
+    commas = array[0]
+  elsif array.length == 2
+    commas = array[0] + " and " + array[1]
+  elsif array.length > 2
+    array.each do |item|
+      if item == array[-1]
+        commas << "and #{item}"
+      else
+        commas << "#{item}, "
+      end
+    end
+  end
+  commas
 end
