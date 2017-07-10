fc071c22b43cfb368a16994a59ca848389e1429c
bleavs <bleavitt@colgate.edu>
2017-07-10 13:34:04 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        "#{array[0]}"
+        
+    elsif array.length == 2
+        "#{array[0]} and #{array[1]}"
+        
+    elsif array.length > 2
+        last = array.pop
+        response= array.join(", ")
+        response += ", and #{last}"
+        response
+    end
 end
