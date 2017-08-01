c9c545c08b5ead86da61b248d125c69696e43d50
Ashlee Crusco <ashleecrusco@gmail.com>
2017-08-01 18:15:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else
+    last = array.pop
+    array << "and "
+    array.join(", ") + last
+  end
 end
