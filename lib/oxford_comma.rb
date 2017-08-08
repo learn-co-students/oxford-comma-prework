107bab26e6c19f1c70f6d12aceeff9773bb89ce4
Cole Shapiro <colesayershapiro@gmail.com>
2017-08-08 04:28:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+return array.join if array.length == 1
+return array.join(" and ") if array.length == 2
+  if array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
