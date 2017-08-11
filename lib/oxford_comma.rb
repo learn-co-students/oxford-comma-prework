9c64d1b96e0eb29e095960c703913bc2b640924e
Ashe Austaire <AsheAustaire@gmail.com>
2017-08-11 10:52:12 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return ["kiwi"].join
+    elsif array.length == 2
+        return ["kiwi", "durian"].join(" and ")
+    elsif array.length >= 3
+        array[0...-1].join(", ") <<", and #{array[-1]}"
+    end
 end
