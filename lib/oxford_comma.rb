d9367b08528d08e3cc0455cbc461c44a4dc78411
jonopens <jonopenshaw@gmail.com>
2017-08-12 15:34:56 -0400

    that vampire weekend song was pretty good.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+	case
+	when array.size == 1
+		return array[0]
+	when array.size == 2
+		return array.join(" and ")
+	when array.size >= 3
+		last_element = array.last
+		array.pop
+		array.push("and #{last_element}")
+		return array.join(", ")
+	else
+		return "array must contain at least one element"
+	end
 end
