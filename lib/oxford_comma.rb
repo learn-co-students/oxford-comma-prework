0d5af5ada5047a66491499131e55d90e29cce77e
Rebecca Newborn <rjn58@cornell.edu>
2017-08-21 15:09:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    output = ""
+    counter = 0
+    while counter < array.size - 1
+      output << "#{array[counter]}, "
+      counter += 1
+    end
+    output << "and #{array[-1]}"
+    return output
+  end
 end
