0b501682b64ebf008546ff3e4aad76b80f8bfe7a
Robert Peck <rspeckpfs@gmail.com>
2017-08-27 01:29:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma(array) #This was so much easier than the same kab in javascript. Is ruby
+  output = ""
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    final_element = array.pop
+    string_array = array.join(", ")
+    string_array << ", and #{final_element}"
+  end
 end
