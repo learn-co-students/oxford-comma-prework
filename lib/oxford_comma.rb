dc8584f3c10078ff83385047394bf996e1f01d3e
Paul Kristapovich <paulkristapovich@gmail.com>
2017-09-02 20:54:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+def two_words(array)
+  return array[0] + " and " + array[1]
+end
 
+def oxford_comma(array)
+  string = ""
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2
+    return two_words(array)
+  else
+    while (array.length > 2)
+      string << array[0]+", "
+      array.shift
+    end
+    string << array[0] + ", and " + array[1]
+  return string
 end
