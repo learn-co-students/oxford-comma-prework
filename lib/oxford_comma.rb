490782e2172daa649781d616f8c580877cfc5ac9
Dakota Lillie <bewguy101@gmail.com>
2017-09-15 14:32:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  phrase = ""
+  if array.size > 2
+    array.each do |word|
+      if array.index(word) < array.size - 1
+        phrase << "#{word}, "
+      elsif array.index(word) == array.size - 1
+        phrase << "and #{word}"
+      else
+        puts "something went wrong"
+      end
+    end
+    phrase
+  elsif array.size <= 1
+    phrase << array.join
+  elsif array.size == 2
+    phrase << array.join(" and ")
+  end
 end
