f3e5e5dd06088b5838493708bad090a30b0bab57
Alona Yeheskel <alona.yeheskel@gmail.com>
2017-09-18 01:47:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  string = ""
+  if array.size == 1
+    return array.join
+  elsif
+    array.size == 2
+    return array.join(" and ")
+  else last = array.pop
+    array.each do |ele|
+    string += "#{ele}, "
+    end
+    return string += "and #{last}"
+  end
 end
