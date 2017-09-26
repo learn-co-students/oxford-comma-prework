58324b613a4148ad3adcafbf570ef5c8442f7ce7
George Gore <georgelgore@gmail.com>
2017-09-26 20:41:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return array.first
+  elsif array.count == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.count == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    first_half = array[0...-1].join(", ")
+    first_half << ", and #{array[-1]}"
+  end
 end
