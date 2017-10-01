3090987365376e390211444cfff396e205636e20
Jake <jakemacnaughton@gmail.com>
2017-10-01 12:40:51 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    new_array = []
+    array.each_with_index do |el, i|
+      if i == array.length-1
+        new_array << "and #{el}"
+      else
+        new_array << el
 
-end
+      end
+    end
+    new_array.join(", ")
+  end
+end  
