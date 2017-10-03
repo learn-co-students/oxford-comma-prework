f38c28b1704b5dae09e12ee231bc3af193d48615
github username <github email address>
2017-10-02 23:07:37 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.first
+  elsif array.size == 2
+    return "#{array.first} and #{array.last}"
+  elsif array.size >= 3
+    string = ""
+    for index in 0..array.size do
+      if index == array.size - 1
+        string << "and #{array[index]}"
+        return string
+      else
+        string << "#{array[index]}, "
+      end
+    end
+    return string
+  end
 end
+
+#multiple = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+#two = ["kiwi", "durian"]
+#puts multiple.size
+
+#oxford_comma(multiple)
