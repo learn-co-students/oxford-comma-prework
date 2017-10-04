726ed479b0e48d091652d14a3b0cf17590a7f601
JoshD1 <joshdeiner75@gmail.com>
2017-10-04 11:35:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array = nil)
   if array.size == 1
     return array.join(" ")
   elsif array.size == 2
-    return array.to_s if array.nil? || array.length <= 1
+
     array[0..-2].join(", ") + " and " + array[-1]
   else
-  return array.to_s if array.nil? || array.length <= 1
+
   array[0..-2].join(", ") + ", and " + array[-1]
 end
 end
