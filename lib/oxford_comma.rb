77a208c1a57718c5b34d3019a427963ba082ba0e
eal6gf <elum1234@gmail.com>
2017-10-28 02:29:43 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+case array.size
 
+when 1
+  return array[0]
+when 2
+  return array[0] + " and " + array[1]
+else
+  last_item = array.pop
+  ret_string = array.join(", ") + ","
+  ret_string = ret_string + " and " + last_item
+end
 end
