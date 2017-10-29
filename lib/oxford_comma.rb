839454155646269076f4805534855e126ee4a8a7
krdiamond <github email address>
2017-10-29 18:03:13 -0400

    solved lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+    if array.length < 3
+        array.join(" and ")
+    else array.length >= 3
+        (array.insert(-2, "and " ))[0..-2].join(", ") + array[-1]
+    end
 end
+
