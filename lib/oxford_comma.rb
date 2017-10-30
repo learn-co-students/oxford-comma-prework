b7f24513072a5a8a335b20e795587da48f5c73ce
humzahchoudry <Humzah.choudry@gmail.com>
2017-10-30 10:56:43 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array[0]
+   elsif array.length == 2
+     return "#{array[0]} and #{array[1]}"
+  # else
+  #   return "#{array[0, array.length-1].join(', ')} and #{array.last}"
+   end
+counter = 0
+  while counter < array.length - 1
+    array[counter] << ", "
+    counter += 1
+  end
+return "#{array[0, array.length-1].join}and #{array.last}"
 
 end
