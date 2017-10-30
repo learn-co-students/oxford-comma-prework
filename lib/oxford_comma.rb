e8b10d67ac35ddc256316dea264a1da05116dfe2
David Tenenbaum <david.tenenbaum87@gmail.com>
2017-10-30 22:48:19 +0000

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join("")
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    result = ""
+    array[0...-1].each { |x| result << x + ", "}
+    result << "and #{array[-1]}"
+    return result
+  end
 end
