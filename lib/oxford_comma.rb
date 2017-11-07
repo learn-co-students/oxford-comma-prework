54de3f9f1078fe21554570d216ed7f7cb313b06c
nspp99b <chromepanthers@gmail.com>
2017-11-07 00:32:00 -0500

    solved using pop

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 0
+    nil
+  elsif array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    oxford = array.pop
+    array.join(", ") + ", and " + oxford
+  end
 end
