48f9a7448a3558ca17be2c7a52df3880f91ccedb
David Welch <dmw9400@gmail.com>
2017-11-10 16:16:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,11 @@ def oxford_comma(array)
     return array.join
   elsif array.length == 2
     return array.join(" and ")
+  elsif array.length > 2
+    array2 = []
+    array2.push(array[0...-1])
+    return_string = array2.join(", ")
+    last_item = ", and #{array[-1]}"
+    return return_string + last_item
   end
 end
