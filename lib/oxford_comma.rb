5dc8805d65ce2a7760d89e85de885af3b4c2130f
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-13 19:23:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+output=""
+  if array.size==1
+    output=array.join()
+  elsif array.size==2
+    output=array.join(" and ")
+  else
+    array.insert(-2, "and").join(", ").sub("and,", "and")
+  end
 end
