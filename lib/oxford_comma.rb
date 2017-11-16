720dea20d7a9de5ea364b541fb72542c7fd7ef5a
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-16 20:02:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count == 3
+    "#{array[0,2].join(", ")}, and #{array.last}"
+  else
+    "#{array[0,array.count-1].join(", ")}, and #{array.last}"
+  end
 end
