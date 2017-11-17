7e3ea39d981a199909c427a7ec3284f1b8601823
Kenny Hom <kenny.hom27@gmail.com>
2017-11-17 15:53:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.first
+  elsif array.count == 2
+    array.first + " and " + array.last
+  elsif array.count > 2
+    array[0...-1].join(", ") + ", and #{array.last}"
+  end
 end
