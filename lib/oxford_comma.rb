104d357143c23e95ef69706f44f5c92ca91ffd38
Dwire <gjdwyer8@gmail.com>
2017-12-17 23:33:35 -0500

    Pass all spec tests

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  final_two = ["and"]
 
+  if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    final_two << array.pop
+    working_array = array.map { |chr| chr + "," }
+
+    working_array.push("").join(" ").concat(final_two.join(" "))
+end
 end
