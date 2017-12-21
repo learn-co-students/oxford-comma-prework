325be006a43788356bd828be811aa5354280a7ef
Melissa Nock <melnock@gmail.com>
2017-12-21 17:54:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size ==1 then
+    array.join
+  elsif array.size == 2 then
+    array.join(" and ")
+  else 
+    array_end = array.pop
+    comma_array = array.join(", ")
+    comma_array << ", and #{array_end}"
+  end
 end
