36a8f90c517afbae29751bc386139e3e64756bef
scottburack <itsmescottb123@gmail.com>
2017-12-29 20:06:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,5 @@
 def oxford_comma(array)
-
+  return array[0] if array.size == 1
+  return "#{array[0]} and #{array[1]}" if array.size == 2
+  array.insert(-2, "and").join(", ").sub("and,", "and")
 end
