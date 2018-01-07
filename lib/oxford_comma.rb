eeba9bb74eeacd77b1187d68a0cb135ebf442309
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2018-01-07 19:17:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
 
+  string = ""
+
+  if array.count == 1
+    array.first
+  elsif array.count == 2
+    array.join(" and ")
+  
+  else
+    array.insert(array.count-1, "and")
+    array.each_with_index do |element, index|
+      if index == array.count-2
+        string << element.concat(" ")
+      elsif index == array.count-1
+        string << element
+        return string
+      else
+        string << element.concat(", ")
+      end
+    end
+  end
+
 end
