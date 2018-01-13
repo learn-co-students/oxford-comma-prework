84a75295742342ef55a22f028f92901b1826f0bf
allinthemind66 <r.nilon92@gmail.com>
2018-01-13 17:12:05 -0500

    Oxford Comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length < 3
+    array.join(" and ")
+  else
+  array[array.length-1].prepend("and ")
+  array.join(", ")
+  end
 end
