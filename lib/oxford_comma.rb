7d746a345f3660a54938e50e7547c1e92f17cd66
Barney Fabian <bafa@tutanota.de>
2018-01-13 22:00:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  
+  array2 = array.take (array.length-1)
+  string = array2.join(", ")
+  
+  if array.length == 2
+  string << " and " 
+  
+  elsif array.length > 2
+  string << ", and "
+  
+  end
+  
+  string << array.last
+  string
+  
 end
