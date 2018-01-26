49bb85d2302fd82af13bb33ad5ba31cb1b8622b8
Helen Liutongco <liutongcoh@gmail.com>
2018-01-25 18:24:10 -0600

    Modify oxford_comma.rb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  string = array[0]
+  if array.length == 2
+    string << " and #{array[1]}"
+  elsif array.length > 2
+    index = 1
+    until index >= array.length - 1 do
+      string << ", #{array[index]}"
+      index += 1
+    end
+    string << ", and #{array[-1]}"
+  end
+  string
 end
