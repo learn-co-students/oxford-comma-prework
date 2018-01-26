e4b07ed5ee5097efbc8e6ab16bfa160d1ed69b54
Helen Liutongco <liutongcoh@gmail.com>
2018-01-25 18:38:17 -0600

    Modify oxford_comma.rb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,8 @@ def oxford_comma(array)
   if array.length == 2
     string << " and #{array[1]}"
   elsif array.length > 2
-    index = 1
-    until index >= array.length - 1 do
-      string << ", #{array[index]}"
-      index += 1
-    end
-    string << ", and #{array[-1]}"
+    array[-1].insert(0, "and ")
+    string = array[0..array.length-1].join(", ")
   end
   string
 end
