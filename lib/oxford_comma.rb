0a2868d3cf11d326d28656ba7a7e0b8281c93077
nkalkstein <nkalkstein@gmail.com>
2018-01-29 20:59:25 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  formatted = ""
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(' and ')
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  end
+  while array.length > 3
+    i = 0
+    while i < array.length - 1
+      formatted << "#{array[i]}, "
+      i += 1
+    end
+    while i < array.length
+      formatted << "and #{array[i]}"
+      i += 1
+    end
+    return formatted
+  end
 end
