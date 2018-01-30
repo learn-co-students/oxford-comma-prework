f7706484dc56b8e72195b208530da2ff9d5765b1
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-01-30 22:56:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array = array.join("")
+  when 2
+    array[array.length - 1] = "and " + array[array.length - 1]
+    array = array.join(" ")
+  else
+    array[array.length - 1] = "and " + array[array.length - 1]
+    array = array.join(", ")
+  end
+  array
 end
