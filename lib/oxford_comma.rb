cf1e9273e69f4ad8a50e58a4e67c7adaaf1d8948
Ray Blasko <mray6288@gmail.com>
2018-02-04 23:48:49 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    string = ""
+    if array.length == 1
+        string = array.shift
+    elsif array.length == 2
+        string = array.join(' and ')
+    else
+        while array.length > 1
+            string << "#{array.shift}, "
+        end
+        string << "and #{array.shift}"
+    end
+    string
 end
