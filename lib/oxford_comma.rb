e3b0d0b1916adeaff26cbedab4f76905046139db
William Dale <williambdale@gmail.com>
2018-03-05 18:35:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2 
+    array.join(' and ')
+  elsif array.length < 2
+    array.join
+  else 
+    last_word = array.pop 
+    array << "and #{last_word}"
+    array.join(", ")
+  end 
 end
