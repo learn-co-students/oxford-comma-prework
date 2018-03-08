b03e6ed852370ec7b1490531efc9046b276295cc
laura-campbell <campbell.laura.elizabeth@gmail.com>
2018-03-08 09:49:51 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.count == 2
+        string = array.join(" and ")
+    elsif array.count == 1
+        string = array[0]
+    elsif array.count > 2
+        string = array[0..-2].join(", ")
+        string << ", and #{array[-1]}"
+    else
+        string = nil
+    end
+    return string
 end
