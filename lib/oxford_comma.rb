01792e9082d88cb6c22cdc8a4ab8fd16d967c3b5
LaneMiller Miller <chris.lane.miller@gmail.com>
2018-03-20 01:30:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  array.each do |item|
+    if array.length == 1
+      array.join
+    elsif array.length == 2
+      array.join(" and ")
+    elsif array.length > 2
+      array[0...array[-1]].join(", ")
+      array[-1].join(", and ")
+    end
+  end
 end
