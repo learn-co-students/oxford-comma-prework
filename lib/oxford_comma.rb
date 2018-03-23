7d75ba0b7db7fe39d27d0ce4e5b417168e6bd365
Joe Greiner <jrgreiner115@gmail.com>
2018-03-23 16:30:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+if array.count == 1
+  array.join(", ")
+  elsif array.count == 2
+array.join(" and ")
+else
+ox_com = ", and " + array.pop
+string = array.join(", ") << ox_com
+end
 end
