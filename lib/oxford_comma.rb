0ca864812cf33f2f7c339deecc2247486f6bcceb
munirkizilcali <munir.kizilcali@gmail.com>
2018-04-03 13:40:29 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.size == 2
+        array.join(" and ")
+    elsif array.size == 1
+        array[0].to_s
+    else
+        array[-1] = "and #{array.last.to_s}"
+    array.join(", ")
+    end
 end
