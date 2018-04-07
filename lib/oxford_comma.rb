1dd880d0f07a4318fef2d672527b8024a9b169b4
squamuglia <maxsmouha@gmail.com>
2018-04-07 14:51:19 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+	if array.length == 1
+		array.join
+	elsif array.length == 2
+		array.join(" and ")
+	else 
+		b = array.pop
+		array << "and #{b}"
+		array.join(", ")
+	end
 end
