6213512c41505bdbfa716faee97f42f5dfe79ba1
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-09 21:28:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+require "pry"
 def oxford_comma(array)
+  if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.insert(1, "and").join(" ")
+  else
+    last = array.pop
+    string = array.join(", ") + ", and " + last
 
+  end
 end
