7d44e817b69e334fe678c47de0c4d6571e0df06f
JohnZBurnett <tuninator@gmail.com>
2018-04-10 16:21:08 -0400

    Wrote comma methods.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  str_w_comma = ''
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    str_w_comma = array[0] + " and " + array[1]
+  elsif array.length >= 3
+    array.each_with_index do |str, idx|
+      if idx < array.length - 1
+        str_w_comma += array[idx] + ", "
+      else
+        str_w_comma += "and " + array[idx]
+      end
+    end 
+    str_w_comma
+  end
 end
