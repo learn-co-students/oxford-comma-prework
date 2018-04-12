2e3528c17d4654474d9edf73d32207dcd3787631
Daniel Shimon <danielzshimon@gmail.com>
2018-04-12 17:27:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length > 2 
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  elsif 
+    array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else 
+    array.join
+  end
 end
