41852d5ac8008144927eb082352fcbb17a5012a5
Chris Alberti <christopherjalberti@gmail.com>
2018-04-19 00:03:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
 
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(' and ')
+  else
+    result = ""
+    array.each_with_index do |ele, idx|
+      if idx == array.length - 1
+        result += "and #{ele}"
+      else
+        result += "#{ele}, "
+      end
+    end
+    result
+  end
 end
