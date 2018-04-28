586bdfbbc2f77bd71048ba1da4aa4a477f3f75bd
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 01:27:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(' and ')
+  elsif array.size == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.size > 3
+    last_item = array.pop()
+    return array.join(", ") + ", and " + last_item
+  end
 end
