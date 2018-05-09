e5f2ccb1c6d8ec2080c2421398f8edf3ce41e58c
jchamberlain909 <jchamberlain909@gmail.com>
2018-05-09 15:29:34 -0400

    implemented oxford_comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if(array.size>2)
+    last_element = array.pop
+    return array.join(", ") +", and #{last_element}"
+  elsif array.size==2
+    return array.join(" and ")
+  else
+    return array.join
+  end
 end
