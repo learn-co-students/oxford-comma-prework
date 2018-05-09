f54fb83df69319eb84ea71fdb1e9aa775be6c9e5
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-09 10:27:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.insert(-2, "and")
+    string = array.join(", ")
+    last_occurance = string.rindex(",")
+    string.slice!(last_occurance)
+    string
+  end
 end
