5f24f63b5dc15c365a1fba83fab6e8b3bbb9e4e4
Jigar Polra <jigarpolra@live.co.uk>
2018-05-14 16:57:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+     return "#{array[0]}"
+   when 2
+     return array[0..array.length].join(" and ")
+   else
+     str = array[0..array.length - 2].join(", ") + ", and #{array[array.length-1]}"
+     return str
+   end
 end
