21ed5fc8fe245bb7998cabff0d829c7366c73027
Stephen F <stephen@farver.io>
2018-05-27 22:57:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  output = ""
+  if array.length > 2
+    array.each_with_index do |el, i|
+      if i == array.length - 1 
+        output.concat("and #{el}")
+      else
+        output.concat("#{el}, ")
+      end
+    end
+  elsif array.length == 2 
+    output = "#{array[0]} and #{array[1]}"
+  elsif array.length == 1
+    output = array.first
+  end
+  
+  output
 end
