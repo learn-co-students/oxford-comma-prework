2bc06bb4669985cf262c28bfac1ae3c96bf6c239
Gabriel.Batista <gabedbatista@gmail.com>
2018-05-30 12:05:03 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
 
+  if array.size == 1
+    array.join
+
+  elsif array.size == 2
+    array.join(" and ")
+
+  elsif array.size >= 3
+    result = ""
+    array.each do |name|
+      name == array.last ? result << "and #{name}" : result << "#{name}, "
+    end
+    result
+  end
+
+
 end
