d8e646ffc919ba0eeeeb2b0476340f104fa0d30a
yunglleung1 <yungl.leung1@gmail.com>
2018-06-02 16:34:18 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma(arrayOfStrings)
+  if arrayOfStrings.length() == 1
+    return arrayOfStrings.join()
+  elsif arrayOfStrings.length() == 2
+    return arrayOfStrings.join(" and ")
+  else
+    lastElement = arrayOfStrings.pop()
+    modifiedLastElement = "and " + lastElement
+    arrayOfStrings.push(modifiedLastElement)
+    return arrayOfStrings.join(", ")
+  end
 end
