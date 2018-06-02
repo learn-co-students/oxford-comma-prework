e668cfac580e70ad93c32e201a24866273183eb9
jaredianmills <jaredianmills@gmail.com>
2018-06-02 12:26:56 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  string = ""
+  if array.size == 1
+    string = "#{array[0]}"
+  elsif array.size == 2
+    string = "#{array[0]} and #{array[1]}"
+  else
+    last_element = array.pop
+    array.each{|element| string += "#{element}, "}
+    string += "and #{last_element}"
+  end
 end
