b57af9534988f8eb9ad0d7c4f8789741db27ead0
Jason Pitts <juniorjr083@gmail.com>
2018-06-07 01:54:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+if array.length == 1 
+  return array[0]
+  
+elsif array.length == 2
+ array.join(" and ")
+ 
+ else
+   ans = array.pop
+ array.insert(-1, "and ")
+   array[0..-1].join(", ") + ans.to_s
+   
+end 
+ 
+
+  
 
 end
+
