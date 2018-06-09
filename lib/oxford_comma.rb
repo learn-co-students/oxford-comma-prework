eda468d0aff132997f0ecf84b87eb21d47f64502
cmcorrales <cmcorrales3@gmail.com>
2018-06-09 09:10:02 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join("")
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size >= 3
+    #add "and" between elements 2 and 3
+    last_element = array.pop()
+    string = array.join(", ")
+    string << ", and #{last_element}"
+    string
+    #convert entire array into a string
+  end
 end
