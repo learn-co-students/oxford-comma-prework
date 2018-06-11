dabf89d4af1b16ae6cfbcecffcecc1961f3b5c26
Yicheng Jin <bobjin42@gmail.com>
2018-06-11 18:37:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length < 2;
+    return array.join(" ")
+  elsif array.length == 2;
+    return "#{array[0]} and #{array[1]}" 
+  else 
+    last_ele = array[-1]
+    new_last_ele = ", and #{last_ele}"
+    array.pop
+    return array.join(", ") + new_last_ele
+  end
 end
