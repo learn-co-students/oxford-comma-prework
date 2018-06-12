446314dec91a73d16be2011660fa412c395665cd
Michael Peterson <mik3wk@gmail.com>
2018-06-12 15:01:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length >= 3
+  array2 = []
+  array.each do |x| array2.push(x)
+  end
+  array2.pop
+  array2 << "and #{array.last}"
+  return array2.join(", ")
+  end
+
 
 end
