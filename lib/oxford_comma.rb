d68ca07d0703f549a00b54b2868b600c1b14b033
github username <github email address>
2018-06-16 16:32:31 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  case array.length
+  when 1
+    return array[0]
+  when 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    longer_than_two(array)
+  end
+end
 
+def longer_than_two(array)
+  returned = ""
+  length = array.length - 1
+  array.each_with_index do |el, idx|
+    idx != length ? returned << "#{el}, " : returned << "and #{el}"
+  end
+  returned
 end
