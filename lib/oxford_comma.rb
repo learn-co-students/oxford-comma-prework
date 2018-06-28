8ca0a26d3260ac5fe2b4972e3c1b8f3ec6637b61
Dina Hafez <dinahafez.8@gmail.com>
2018-06-28 22:33:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def oxford_comma(array)
-
+  if array.length == 1
+    puts array.join
+  elsif array.length == 2
+    puts array.join(" and ")
+  else
+    last = array.pop
+    puts "#{array.join(", ")}, and #{last}"
+  end
 end
