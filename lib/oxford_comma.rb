13834936ad0e70e9708bc207400f6c3bfb631a76
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 22:22:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  new_array = []
+  i = 0
+  if array.length == 1
+    array[0]
+  elsif array.length<=2
+    array.join (" and ")
+  else
+    while i<array.length - 1
+    new_array<<array[i]+", "
+    i += 1
+    end
+    
+  end
 end
+  
