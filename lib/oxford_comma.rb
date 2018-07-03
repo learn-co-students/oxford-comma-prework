cabe395282e1c58957cbefebb0a57c16f077483d
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-03 16:23:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  oxford = ''
+  if array.length == 1
+    oxford = array[0]
+  elsif array.length == 2
+    oxford = array[0] + " and " + array[1]
+  else
+    oxford = array[0, array.length-1].join(', ') + ", and " + array[-1]
+  end
+  oxford
 end
