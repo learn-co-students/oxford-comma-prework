b3e0639823e0979d2cc56a957013c588d69080f5
Ciaran Morinan <cmorinan@gmail.com>
2018-07-06 21:02:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1 then
+    array
+  elsif array.length == 2 then
+    array.join " and "
+  else
+    array[-1] = "and " + array[-1]
+    oxford_string = array.join(", ")
+  end
 end
