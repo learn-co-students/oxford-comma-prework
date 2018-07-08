8ec469ce37797f0b141a1e12c65b8290ae2e280a
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-08 17:12:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+  
+  if array.length == 1 
+    return array.join
+    # returns string
+    
+  elsif array.length == 2 
+    return array.join(" and ")
+    #adds "and" between strings
+    
+  elsif array.length >= 3
+    last_element = array.last
+    array.pop
+    array.insert(-1, "and " )
+    new_element = array.join(", ")
+    final_element = new_element + last_element
+    return final_element
+    #adds commas and "and" before last string 
+    
+  end 
 end
