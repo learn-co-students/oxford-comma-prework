0e470f5cf61bb0749d3785342fe9e55eb5ea02e0
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-07-08 17:39:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  return array.join if array.length == 1
+  return array.join(" and ") if array.length == 2
+  if (array.length > 2)
+    final_element = array.pop
+    array = array.join(", ")
+    return "#{array}, and #{final_element}"
+  end
 end
