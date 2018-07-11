5efc2a5aa086a181f644285c3021a563655b3906
Michael Coleman <michaelcoleman19@gmail.com>
2018-07-11 16:05:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  if array.count == 1
+    array.join()
+  elsif array.count == 2
+    array.join(' and ')
+  elsif array.count == 3
+    array.each do |name|
+      counter = 0
+      while counter < 2
+        array.join(', ')
+      end
+        array.join(' and ')
 
+end
+  end
 end
