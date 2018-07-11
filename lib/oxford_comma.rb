fb4fc873e1855eadff1145786d6443c2ba0635a2
Michael Coleman <michaelcoleman19@gmail.com>
2018-07-11 17:09:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,15 @@
 def oxford_comma(array)
+  string = ''
   if array.count == 1
     array.join()
   elsif array.count == 2
     array.join(' and ')
-  elsif array.count == 3
-    array.each do |name|
-      counter = 0
-      while counter < 2
-        array.join(', ')
-      end
-        array.join(' and ')
-
-end
+  elsif array.count > 2
+    for variable in 0..array.count-2 do
+      string.concat("#{array[variable]}, ")
+    end
+      string.concat("and #{array[array.count-1]}")
+  ##  end
+    return string
   end
 end
