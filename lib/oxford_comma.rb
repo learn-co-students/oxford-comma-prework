4736a3f4beca91891817818676f8e13724018836
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-22 21:58:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length==1
+    return array.join
+  else
+    array[-1]='and '+array[-1]
+    if array.length==2
+       return array.join(' ')
+    else 
+      return array.join(', ')
+    end
+  end
 end
