7ed0c022bbbaf89e6e7136aca603f0c69c4c32ff
Chribtoper <xcstolfax@gmail.com>
2018-08-02 22:58:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  end
+last_ele = array.last
+array.pop
+array.insert(array.size, "and #{last_ele}")
+if array.size == 2
+  newString = array.join (" ")
+else
+newString = array.join(", ")
+end
+newString
 end
