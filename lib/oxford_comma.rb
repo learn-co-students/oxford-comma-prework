df713b6f197c41a1d7e03aa07ce90b24504e7e54
Jin Ai Yap <jinai.jyap@gmail.com>
2018-08-04 00:23:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.length
+    when 1
+      array[0]
+    when 2
+      "#{array[0]} and #{array[1]}"
+    else
+      last = array.pop
+      "#{array.join(", ")}, and #{last}"
+  end
 end
