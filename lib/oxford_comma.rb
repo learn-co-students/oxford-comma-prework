c4d1078d747902f0da8e20d492f55bc3abecea87
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-10 19:38:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
-  new_array = array.pop
-  string = new_array.join(", ") 
-  string << ", and #{array[-1]}"
-  string
-end
+  if array.length == 1 
+   "#{array[0]}"
+  end
+  
+end 
+
