e0df214ec5a8cf6e4da2c025152861511b0ea76c
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-10 20:01:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,15 @@
 def oxford_comma(array)
   if array.length == 1 
-   "#{array[0]}"
+    return "#{array[0]}"
   end
-  
+  if array.length == 2 
+    return "#{array.join(" and ")}"
+  end 
+  if array.length > 2
+    remember = array[-1]
+    array.delete_at(-1)
+    string = array.join(", ")
+    string << ", and #{remember}"
+  end 
 end 
 
