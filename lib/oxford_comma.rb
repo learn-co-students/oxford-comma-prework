d5eb53fdb6befafb199e710c3991833aaea75ad6
Billie Simmons <billiesnyc@gmail.com>
2018-08-15 13:58:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+    array.join(" ")
+    elsif array.length == 2 
+    array.join(" and ")
+    elsif array.length >= 3
+    last_item = array.pop
+    array.push("and #{last_item}")
+    array.join(", ")
+  end
 end
