ff28b8cf4b93315c12b7be998e49497abd279db2
EthanFe <sneakcow@gmail.com>
2018-08-17 14:02:04 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  string = ""
+  array.each_with_index do |item, index|
+    if index == array.length - 1
+      string << item
+    elsif index == array.length - 2
+      if array.length == 2
+        string << item + " and "
+      else
+        string << item + ", and "
+      end
+    else
+      string << item + ", "
+    end
+  end
+  string
 end
