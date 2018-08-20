6514e312d2b144118917018e9e045269d77f7f07
Srikant Kumar Kalaputapu <sriki@Srikants-MBP.home>
2018-08-20 16:59:53 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
+    return array[0] if array.size == 1
+    return array[0] + " and " + array[1] if array.size == 2
 
+    val = array[-1]
+    val = "and " + val
+    array[-1] = val
+    array.join(", ")
 end
