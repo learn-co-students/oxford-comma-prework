53600754d6221d19d08a3c254d667ef43151c025
skarolk <skarolkiewicz@gmail.com>
2018-08-22 05:08:30 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  elsif array.size == 3
+    return (array[0] + ", " + array[1] + ", and " + array[2])
+  elsif array.size >= 4
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
