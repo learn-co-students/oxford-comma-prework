872ad11f433278069dd6167acbef305c6878cacf
anavarreteuk <a.navarrete.uk@gmail.com>
2018-09-02 20:38:37 +0100

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
+
+if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_item = array.pop
+    return "#{array.join(", ")}, and #{last_item}"
   end
-  array.join(", ")
 end
