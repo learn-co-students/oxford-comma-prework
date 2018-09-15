4310da4e1280b6a205d02374583c23a1f67d779b
ihollander <ianholla@gmail.com>
2018-09-15 15:07:51 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  if array.size < 3
+    return array.join(" and ")
+  end
 
+  #join array to comma separated string
+  oxfordized = array.join(", ")
+
+  #insert 'and ' after last comma
+  oxfordized.insert(oxfordized.rindex(",") + 2, "and ")
 end
