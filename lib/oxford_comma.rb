29b13cf1517ef9f41249ed0310c0b1571b03e785
yoshinator <yoanante@gmail.com>
2018-09-19 20:09:46 -0400

    Oxford shirt commas

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  string_builder =""
+  if array.size == 1
+    string_builder << array[0]
+  elsif array.size == 2
+    string_builder << array[0] << " and " << array[1]
+  else
 
+    array.each_with_index do |string, index|
+      if index +1 < array.size
+        string_builder << string << ", "
+      else
+        string_builder << "and " << string
+      end
+    end
+    string_builder
+  end
 end
