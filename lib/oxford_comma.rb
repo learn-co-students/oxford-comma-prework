17b2de6a0806bca9b266c48735f53712c3091584
no-relation <eddiec76@gmail.com>
2018-09-25 09:02:30 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        string_end = array.pop
+        string = array.join(", ")
+        string = string + ", and " + string_end  
+    end
 end
