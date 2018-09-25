cf2ac0408e9d817177c98eb8881b362d542af716
vera protopopova <vera.protopopova@gmail.com>
2018-09-25 15:32:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
+def oxford_comma (arr)
+  if arr.length <= 2 
+    str = arr.join(" and ")
+  else 
+    str = arr[0...arr.size - 1].join(", ")
+    return "#{str}, and #{arr[-1]}"
+  end 
+  return str
+end 
 
-end
+oxford_comma(['h', 'e', 'y'])
