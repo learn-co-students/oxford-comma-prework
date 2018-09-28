0b72940c473d332f13d42636ac860b5a43e5742f
Jacob Menashi <jbmenashi@gmail.com>
2018-09-28 15:53:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    new_string = ""
+    array.each do |name|
+      if array.size > 1
+        new_string << "#{name}, "
+        array = array.slice(1, array.size-1)
+      else
+        new_string << "and #{name}"
+      end
+    end
+    new_string
+  end
 end
+
