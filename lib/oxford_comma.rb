4ebcacdc1fc3e884f1cccb32b30ecc8d486a5ec8
Jacob Menashi <jbmenashi@gmail.com>
2018-09-28 16:33:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,19 +1,11 @@
 def oxford_comma(array)
-  if array.size == 1
-    array.join
-  elsif array.size == 2
-    array.join(" and ")
-  elsif array.size > 2
-    new_string = ""
-    array.each do |name|
-      if array.size > 1
-        new_string << "#{name}, "
-        array = array.slice(1, array.size-1)
-      else
-        new_string << "and #{name}"
-      end
-    end
-    new_string
+  case array.size 
+    when 1 
+      array.join
+    when 2 
+      array.join(" and ")
+    else 
+      array[-1] = "and #{array[-1]}"
+      array.join(", ")
   end
 end
-
