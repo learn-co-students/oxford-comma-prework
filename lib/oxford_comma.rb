433e48216b78c68bc40627a6b30c62b77621ca82
Gabrielle Bellini <gbellini90@gmail.com>
2018-10-04 20:50:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length==1
+  return array.join()
+  end
+  if array.length==2 
+    return array.join(' and ')
+end
+if array.length>=3
+  array[-1].insert(0,"and ")
+  array.join(', ')
+end
 end
