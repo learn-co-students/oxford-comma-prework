e1bee9d920baca39bd24e6cffc46e5188f32df70
jbharwood <josephblakeharwood@gmail.com>
2018-10-07 14:53:48 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size < 3
+    string = array.join(" and ")
+  else
+    i = 1
+    while i < array.size do
+      array.insert(i, ", ")
+      i += 2
+    end
+    array.insert(-2, "and ")
+    array.join()
+  end
 end
