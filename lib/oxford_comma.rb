c7a219a9a28d745d77b543c5598dae2fe6ecf510
leannemcabey <leanne.m.cabey@gmail.com>
2018-10-10 20:15:59 -0400

    completed #oxford_comma method to pass tests

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array[-2] << ", and #{array[-1]}"
+    array.pop
+    array.join(", ")
+  end
 end
