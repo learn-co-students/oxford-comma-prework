b8ebfcc4b9aee72e5ed913579a4c655b05670804
Robert Schulte <schulte.robertw@outlook.com>
2018-10-11 17:31:19 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    combined = ""
+    array.each do |word|
+      if word == array.last
+        combined << "and #{word}"
+      else
+        combined << "#{word}, "
+      end
+    end
+    combined
+  end
 end
+
+# oxford_comma(["a","b","c"])
