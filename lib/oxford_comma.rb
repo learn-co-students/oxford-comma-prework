41b2de1c97be2edf6c33d6bc2a25621ec299bc56
artismarti <artismarti@gmail.com>
2018-10-15 17:35:13 +0100

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.push(" and #{array[(array.length)-1]}")
+    array.delete(array[(array.length)-2])
+    array.join
+  else
+    array.push("and #{array[(array.length)-1]}")
+    array.delete(array[(array.length)-2])
+    array.join(", ")
+  end
 end
