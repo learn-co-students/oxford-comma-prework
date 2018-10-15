f25b3156bc8d2979fe2c974af51438629df92962
artismarti <artismarti@gmail.com>
2018-10-15 17:38:26 +0100

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,7 @@ def oxford_comma(array)
   if array.length < 2
     array.join
   elsif array.length == 2
-    array.push(" and #{array[(array.length)-1]}")
-    array.delete(array[(array.length)-2])
-    array.join
+    array.join(" and")
   else
     array.push("and #{array[(array.length)-1]}")
     array.delete(array[(array.length)-2])
