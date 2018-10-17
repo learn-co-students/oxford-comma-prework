183b0b984b5268960aaa5ae2db4e9a9110331d97
artismarti <artismarti@gmail.com>
2018-10-17 13:30:14 +0100

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,9 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-    array.join(" and")
+    array.join(" and ")
   else
-    last = array.pop
-    array.push("and " + "#{last}")
+    array[-1] = ("and " + "#{array[-1]}")
     array.join(", ")
   end
 end
