60ffb01193a9bf274010eec43172a394c8afd51f
Connor Callahan <connorwcallahan@gmail.com>
2018-10-17 17:05:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.size > 2
+      array[-1] = "and " + array[-1]
+      array.join(", ")
+    elsif array.size > 1
+      array.insert(1, "and")
+      array.join(" ")
+    else
+      array.join(",")
+  end 
 end
+
