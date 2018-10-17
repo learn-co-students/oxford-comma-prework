0d28a857fe4709de10a5ca1e9af0d79872ec52c4
artismarti <artismarti@gmail.com>
2018-10-17 23:46:05 +0100

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
-    array[-1] = ("and " + "#{array[-1]}")
-    array.join(", ")
+    (array.slice(0..-2).join(", ")) + (", and " + "#{array[-1]}")
   end
 end
