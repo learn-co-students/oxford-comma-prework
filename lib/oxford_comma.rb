64ebe9d9e0ed56ab345592007b1ff3190508f6f8
joneseyit <joneseyit@protonmail.com>
2018-10-19 17:37:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.length > 2
+    last_el = array.pop
+    string_array = array.join(", ")
+    return "#{string_array}, and #{last_el}"
+  elsif array.length > 1
+    return array.join(" and ")
+  else
+    return array.join
+  end
 end
