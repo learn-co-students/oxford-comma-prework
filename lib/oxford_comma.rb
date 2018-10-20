221c47d2d23d726ee3e8c235212f4bc3f3078e66
IMH51 <ianmartinharrison@outlook.com>
2018-10-20 17:05:28 +0100

    all tests passing

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.first
+  elsif array.size == 2
+    return "#{array.first} and #{array.last}"
+  else
+    string = ""
+    array.each_with_index do |word, index|
+      if index == array.size-1
+        string << "and #{word}"
+      else
+        string << "#{word}, "
+      end
+    end
+  return string
+  end
 end
