c24946097eecf63a69982e77033823e2fbd8c5cb
Tony J. Lum <Tonyjlum@gmail.com>
2018-10-24 21:15:53 -0400

    Oxford Comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  array[-1] = "and #{array[-1]}" if array.length > 1
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" ")
+  else
+    array.join(", ")
+  end
 end
