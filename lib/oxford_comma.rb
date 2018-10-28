6c8137366c52ef1fdf1f8fed15a2c3d564a9fd08
satyasinha94 <satyasinha94@gmail.com>
2018-10-28 14:40:50 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case
+    when array.length == 1
+      array.join
+    when array.length == 2
+      array.join(" and ")
+    when array.length >= 3
+      array[-1].replace("and " + array[-1])
+      array.join(", ")
+  end
 end
