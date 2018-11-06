9ee2d62fc98d761c099e94cb2bfe5db5d81d6e80
Matt Doering <mattdoering86@gmail.com>
2018-11-06 23:04:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    last_word = array.pop
+    words = array.join(", ")
+    words + ", and " + last_word
+  end
 end
