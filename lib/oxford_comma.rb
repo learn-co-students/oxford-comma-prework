3dcb0cc912d7943d79f4c7fa374899a0c7569e04
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-16 20:17:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2 
+    return array.join(" and ")
+  elsif array.length > 2
+    first_part = array[0..-2]
+    last = array[-1]
+    last = "and " + last
+    
+    final_array = first_part.map { |ele| ele + ", "}
+    final_array << last
+end
+return final_array.join("")
 end
