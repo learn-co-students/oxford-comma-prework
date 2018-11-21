8a3d5eec21ffc7759df732688398a6985bc00dfe
cyn7117 <cyn7117@gmail.com>
2018-11-21 03:13:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,3 @@
-# def oxford_comma(array)
-  
-#   if(array.length == 1)
-#   array.join()
-#   elsif
-#   (array.length == 2)
-#     array.join(" and ")
-#   else 
-#   arrayend = array.pop
-#   puts "#{array.join(", ")} and #{arrayend}"
-#   end
-# end
 
 
 def oxford_comma(array)
