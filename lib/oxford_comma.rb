767fc2b4531212ebfe4ec1d5fca6fd754ccc6c12
cyn7117 <cyn7117@gmail.com>
2018-11-21 03:12:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+  
+#   if(array.length == 1)
+#   array.join()
+#   elsif
+#   (array.length == 2)
+#     array.join(" and ")
+#   else 
+#   arrayend = array.pop
+#   puts "#{array.join(", ")} and #{arrayend}"
+#   end
+# end
 
-end
+
+def oxford_comma(array)
+  
+  if(array.length == 1)
+   array.join()
+   elsif
+   (array.length == 2)
+     array.join(" and ")
+   else 
+   arrayend = array.pop
+   puts arrayend
+   newstr = array.join(", ").concat ", and"" #{arrayend}"
+ end
+ end
