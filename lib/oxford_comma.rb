ceb728d05b7aa617c019d393da2c2a14b646ffd9
Anthony Gregg <agregg22@gmail.com>
2018-12-22 21:13:38 -0500

    Complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+if array.length == 1
+  array.join
+elsif array.length == 2
+  array.join(" and ")
+elsif array.length == 3
+  array.insert(2, "and ")
+  array.insert(1, ", ")
+  array.insert(3, ", ")
+  array.join
+elsif array.length > 3
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end
 end
