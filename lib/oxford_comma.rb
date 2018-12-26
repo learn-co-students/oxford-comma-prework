0e774fb59fadf74902f1407a2c0479ceb09538fc
shannon-nabors <shannon.nabors@gmail.com>
2018-12-26 16:47:51 -0500

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.size <= 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    list_segment = []
+    oxford_comma_segment = []
+    list = []
+    list_segment << array[0..array.size-3]
+    oxford_comma_segment << array[array.size-2..array.size-1]
+    list << list_segment.join(", ")
+    list << oxford_comma_segment.join(", and ")
+    return list.join(", ")
+  end
 end
