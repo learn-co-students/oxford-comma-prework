7a76e194fb89f9207e79cb9c321a76f629b9fcef
rrosztoczy <rrosztoczy@gmail.com>
2018-12-26 18:20:09 -0700

    Updated one file

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+if array.size < 2
+    array_string = array.join
+elsif array.size == 2
+    array_string = array.join(" and ")
+elsif array.size >= 3
+    end_of_array = array.pop
+    array_string = array.join(", ")
+    array_string +", and " + end_of_array
+else
+    array.join
+end
 end
