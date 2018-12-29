98da22ae209622a5e0cfc576a32372588c7a3630
Ross Jones <rossmosis.jones@gmail.com>
2018-12-29 15:41:24 -0500

    created method to format different size arrays

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  string = ""
+  if array.size == 1
+    array.first
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    array.each_with_index do |element, index|
+      if index < array.size - 1
+        string += "#{element}, "
+      else
+        string += "and #{element}"
+      end
+    end
+    return string
+  end
 end
