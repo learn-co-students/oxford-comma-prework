cb361fd406cd866b00cef11b012efcde12f30dbd
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-10 00:19:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+number_of_words = array.size
 
+
+ case number_of_words
+    when 1
+      array[0]
+    when 2
+      array.join(" and ")
+    else
+      last_word = array.pop
+      array.join(", ") << ", and #{last_word}"
+  end
 end
