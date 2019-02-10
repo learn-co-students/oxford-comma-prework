e734a9d61b6ad8d6161633cb2cf9a3588b803651
Lisa Scott <lrscot@aol.com>
2019-02-10 03:09:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_item = array.pop
+    array.push("and #{last_item}")
+    array.join(", ")
+  end
+
+
 end
