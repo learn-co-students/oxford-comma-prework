2e7f4dfc6822657d5f9a8c6282f4004b504f3397
ko kristin <koberlander115@gmail.com>
2019-02-17 20:40:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 def oxford_comma(array)
-
+# create empty string to shovels array elements into  
  newString = "" 
  
+# if array has two items, join them with an and 
  if array.length == 2 
-  array.split(" and ")
-  
+  array.join(" and ")
+
+#if an array has three items, add commas to the end of the first two items and a comma and and to the third   
  elsif array.length == 3
-  puts array[0] + ", "
   
+  newString << array.join(", ")
+  binding.pry 
+  
+# this is a variation on the approach for an array of 3  
  elsif array.length > 3
   # what goes here? 
-  
+
+#if the array does not meet any of the criteria before, it is an array of one and can be converted directly to a string   
  else
    array.join
  end
- puts newString
 end
