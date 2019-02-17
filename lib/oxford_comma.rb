51551eed8e19adf409e3e48894dd511633712f9d
ko kristin <koberlander115@gmail.com>
2019-02-17 18:35:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,16 @@
 require 'pry'
 
 def oxford_comma(array)
- array.join 
-  if array.length == 2 
-  puts array[0] + " and " + array
+ if array.length == 2 
+  puts array[0].join + " and " + array
 
-elsif array.length == 3
+ elsif array.length == 3
   puts array[0] + ", "
   
-  else array.length > 3
+ elsif array.length > 3
   # what goes here? 
+  
+ else
+   array.join
  end
 end
