c73fcfdb9dde37c9694ca976049d0afea31b5110
ryanden2018 <denlinger.ryan@gmail.com>
2019-03-05 18:25:24 -0600

    Implemented Oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
+  if array.size == 0
+    return ""
+  end
 
+  if array.size == 1
+    return array[0]
+  end
+
+  if array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  end
+
+  str = ""
+  i = 0
+  while i < array.size-1
+    str << array[i]
+    str << ", "
+    i += 1
+  end
+  str << "and "
+  str << array[i]
 end
