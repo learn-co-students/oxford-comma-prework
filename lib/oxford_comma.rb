da6b56d108785b36e3509d27a9ed63b554d64452
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-15 22:56:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  end
+  if array.size == 2
+    return array.join(" and ")
+  end
+  if array.size >= 3
+    element = array.pop
+    newString = array.join(", ")
+    newString += ", and #{element}"
+    return newString
+  end
 end
