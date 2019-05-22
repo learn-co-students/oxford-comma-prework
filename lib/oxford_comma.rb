56b1af4ce1c5d1ea81854bc8bed5996652351d1f
Ashley Tolen <tolenashley7@gmail.com>
2019-05-22 21:17:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join()
+  elsif array.length == 2
+    array.join(" and ")
+  else # array.length >= 3 (do I need this second half of the line?)
+    array[-1] = "and #{array.last}"
+    array.join(", ")
+    # array.insert(-2, "and")
+    # array = array[0..-1].join(" ")
+    # # first element to second to last element
+    # binding.pry
+  end
 end
