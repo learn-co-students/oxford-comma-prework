575181f8ba3d6bc966d576c71be3ce1f2fbb32c9
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-30 03:49:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length < 3
+    array.join(" and ")
+  elsif array.length >= 3
+    string = ", "
+    first = ""
+    range = (0..array.length-2)
+    for number in array[range] do
+      first = first + number + string
+    end
+    return first + "and " + array[array.length-1]
+  end
 end
