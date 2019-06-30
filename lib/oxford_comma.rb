7e4c6d5b31a2aae336faade61bfbb797c3149b97
Michael Evans <mmevansjr@gmail.com>
2019-06-29 17:46:31 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        return array.join
+    elsif array.size == 2
+        return array.join(" and ")
+    elsif array.size == 3
+        last_word = array.pop
+        phrase = array.join(", ")
+        return "#{phrase}, and #{last_word}"
+    else
+        last_word = array.pop
+        phrase = array.join(", ")
+        return "#{phrase}, and #{last_word}"
+    end
 end
