c792d05472499ec88db44fa65a86aa75b5eb7f1f
Mei Wong <fongmeiw@gmail.com>
2019-10-09 05:41:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  string = ""
+  case array.size
+  when 1
+    string = array.join(",")
+  when 2
+    string = array.join(" and ")  
+  else 
+    i = 0
+    while i < array.size do
+      if i == array.size-1
+        string = "#{string}and #{array[i]}"
+      else
+        string = "#{string}#{array[i]}, "
+      end
+      i += 1
+    end
+  end
+  
+  string
 end
+
