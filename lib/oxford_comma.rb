d713f9f54959617017c59a9a9afe8297e28148ae
dagonyclark <sean@dagony-clark.com>
2019-11-18 18:53:28 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    list = ""
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        counter = 0
+        while counter < array.length - 1 do
+            list << "#{array[counter]}, "
+            counter += 1
+        end
+        list << "and #{array[counter]}"
+    end
 end
