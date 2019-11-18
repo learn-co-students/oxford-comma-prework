b08192f3be5ce9fdd1b8201d5456bc1da012fbfc
dagonyclark <sean@dagony-clark.com>
2019-11-18 18:30:59 -0500

    Revert "Done."
    This reverts commit 7da4c664417dfae55a2197c541f04c3cdc9001e7.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,24 +1,3 @@
-def oxford_comma(array_input)
-  array_to_string = ""
-  counter = 0
-  # array_input.each_with_index do |value, index|
-  #   array_with_commas << array_input[index]
-  # end
-  # array_to_string << array_input[counter]
-  array_to_string << array_input[counter]
-  counter += 1
-  puts array_input.length
+def oxford_comma(array)
 
-  while counter <= array_input.length - 1
-    if array_input.length == 2
-      array_to_string << " and #{array_input[counter]}"
-    elsif counter == array_input.length - 1
-      array_to_string << ", and #{array_input[counter]}"
-    else
-      array_to_string << ", #{array_input[counter]}"
-    end
-    counter += 1
-  end
-
-  return array_to_string
 end
